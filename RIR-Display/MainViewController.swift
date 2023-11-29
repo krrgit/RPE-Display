e94c17b034d0c2e62d7726ad86ea3d5766adc665
kevin <89359821+krrgit@users.noreply.github.com>
2023-11-28 16:35:19 -0800

    Base Functionality Finished

--- RIR-Display/MainViewController.swift
+++ RIR-Display/MainViewController.swift
@@ -15,34 +15,65 @@ protocol OnHeartRateReceivedDelegate: AnyObject {
 class MainViewController: UIViewController, OnHeartRateReceivedDelegate {
     
     @IBOutlet weak var BPMLabel: UILabel!
+    @IBOutlet weak var RIRLabel: UILabel!
+    
+    @IBOutlet weak var maxField: UITextField!
+    @IBOutlet weak var restField: UITextField!
+    @IBOutlet weak var KBToolbar: UIToolbar!
+    
+    
+    
     static let MainView = MainViewController.self
     
     static weak var onHeartRateReceivedDelegate: OnHeartRateReceivedDelegate?
     
     var bpm: Int = 0
+    var restBPM: Int = 60
+    var maxBPM: Int = 140
+    var repsInReserve: Int = 10
+    var ratedPercievedExertion: Float = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         MainViewController.onHeartRateReceivedDelegate = self
+        
+        maxField.inputAccessoryView = KBToolbar
+        restField.inputAccessoryView = KBToolbar
+    }
+    
+    
+    @IBAction func MaxFieldEditEnd(_ sender: Any) {
+        let value = Int(maxField.text ?? "120")
+        maxBPM = max(60, value!)
+        print("🍏 Update Max", String(value ?? maxBPM))
+    }
+    
+    @IBAction func RestFieldEditEnd(_ sender: Any) {
+        let value = Int(restField.text ?? "120")
+        restBPM = max(40, value!)
+        print("🍏 Update Rest", String(value ?? restBPM))
+    }
+    
+    
+    @IBAction func didTapToolbarDone(_ sender: Any) {
+        view.endEditing(true)
+        resignFirstResponder()
     }
     
     func onHeartRateReceived(bpm: Int) {
-        print("🍏", bpm, " BPM")
         self.bpm = bpm
+        calculateRIR()
+        RIRLabel.text = String(repsInReserve)
         BPMLabel.text = String(bpm)
-    }
-    
-    
-    @IBAction func didTapConnectButton(_ sender: Any) {
+        print("🍏", bpm, "BPM | RIR", repsInReserve)
         
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // MARK: - Pass the Central Manager to the View
-
-        // Get access to the detail view controller via the segue's destination. (guard to unwrap the optional)
-        guard let bluetoothViewController = segue.destination as? BluetoothViewController else { return }
-        
+    private func calculateRIR() {
+        let denom = (maxBPM - restBPM) > 0 ? (maxBPM - restBPM) : 1
+        let rawPercent = Float((bpm - restBPM) / denom)
+        ratedPercievedExertion = 10 * rawPercent
+        repsInReserve = 10 - Int(floor(rawPercent))
     }
 }
